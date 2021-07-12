f590adeae6f3203243f3ca31240799d4138d6ae6
pwejar <pwejar@gmail.com>
2021-07-12 13:32:18 +0300

    access3

--- middleware/X2250.js
+++ middleware/X2250.js
@@ -4,13 +4,10 @@ var unirest = require('unirest');
 const getToken = (req, res, next) => {
 
     let auth = "Basic " + Buffer.from(process.env.consumer_key + ":" + process.env.consumer_secret).toString("base64");
-    let jsl = unirest.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials')
+    unirest.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials')
     .headers({'Authorization': auth})
     .send()
     .then((response) => {
-        if(response.error) {
-            return res.status(304).json({message: 'too baa'+process.env.consumer_key})
-        }
         req.access_token = response.body.access_token
         console.log(response.body)
         next()
