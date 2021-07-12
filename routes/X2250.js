a28e83b0eba16cfb5b1b5dc9db2e101d47eaa3aa
pwejar <pwejar@gmail.com>
2021-07-12 21:05:22 +0300

    acesslll2

--- routes/X2250.js
+++ routes/X2250.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const X2250Controller = require('../controllers/X2250Controller')
 const getToken = require('../middleware/X2250')
 
-router.get('/get_tokens',getToken, X2250Controller.getTokens)
+router.get('/get_tokens',getToken, X2250Controller._getTokens)
 router.get('/confirmation', X2250Controller.confirmation)
 router.get('/validation', X2250Controller.validation)
 module.exports = router
