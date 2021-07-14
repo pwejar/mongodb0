6aa895bdb6d3c6ad573315ba57acb7041a157f75
pwejar <pwejar@gmail.com>
2021-07-14 15:11:54 +0300

    PPPP

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 const MenuRoute = require('./routes/menu')
 const AuthRoute = require('./routes/auth')
 const X2250Route = require('./routes/X2250')
-const SmsRoute = require('./controllers/sms')
-var cors = require('cors')
+const SmsRoute = require('./routes/sms')
 
 // mongodb+srv://user0:<password>@db000001.f7dhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGO_PROJECT}?retryWrites=true&w=majority`,{useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://user0:H5YEcZMT5d3XsWk@db000001.f7dhn.mongodb.net/mabuhay?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true})
 // mongoose.connect('mongodb://localhost:27017/testdb',{useNewUrlParser:true, useUnifiedTopology: true})
 const db = mongoose.connection
 
@@ -20,16 +20,15 @@ db.once('open',()=>{
     console.log('welcome home kim')
 })
 const app = express()
-
-app.use(cors()) 
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use('/uploads',express.static('uploads'))
 
 const PORT =process.env.PORT || 3000
 
 app.listen(PORT,()=>{
-    console.log(`kipchoge keep joging, keep  port: ${PORT} `)
+    console.log(`kipchoge${PORT} keep joking, keep choking sfsdf`)
 })
 app.use('/api/menu', MenuRoute)
 app.use('/api/auth', AuthRoute)
