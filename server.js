dfbf649636ebc4fc06719ec305ff1383f3c648c3
pwejar <pwejar@gmail.com>
2021-07-14 15:29:36 +0300

    hero

--- server.js
+++ server.js
@@ -21,9 +21,8 @@ db.once('open',()=>{
 })
 const app = express()
 app.use(morgan('dev'))
-app.use(express.bodyParser());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use('/uploads',express.static('uploads'))
 
 const PORT =process.env.PORT || 3000
